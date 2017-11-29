7c8c5e4b5add6f9063db217c0a1ddac78621f41b
Huy <giahuy0000@yahoo.com.vn>
2017-11-29 12:59:52 +0700

    fix server

--- application.py
+++ application.py
@@ -6,11 +6,10 @@ Author: Scott Rodkey - rodkeyscott@gmail.com
 
 Step-by-step tutorial: https://medium.com/@rodkey/deploying-a-flask-application-on-aws-a72daba6bb80
 '''
+from __future__ import print_function
 
-from flask import Flask, render_template, request
-from application import db
-from application.models import Data
-from application.forms import EnterDBInfo, RetrieveDBInfo
+from flask import Flask, render_template, request, make_response
+import sys
 
 # Elastic Beanstalk initalization
 application = Flask(__name__)
@@ -21,31 +20,15 @@ application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index', methods=['GET', 'POST'])
 def index():
-    form1 = EnterDBInfo(request.form) 
-    form2 = RetrieveDBInfo(request.form) 
-    
-    if request.method == 'POST' and form1.validate():
-        data_entered = Data(notes=form1.dbNotes.data)
-        try:     
-            db.session.add(data_entered)
-            db.session.commit()        
-            db.session.close()
-        except:
-            db.session.rollback()
-        return render_template('thanks.html', notes=form1.dbNotes.data)
-        
-    if request.method == 'POST' and form2.validate():
-        try:   
-            num_return = int(form2.numRetrieve.data)
-            query_db = Data.query.order_by(Data.id.desc()).limit(num_return)
-            for q in query_db:
-                print(q.notes)
-            db.session.close()
-        except:
-            db.session.rollback()
-        return render_template('results.html', results=query_db, num_return=num_return)                
-    
-    return render_template('index.html', form1=form1, form2=form2)
+    resp = make_response(render_template('index.html'), 200)
+
+    if request.method == 'GET':
+        return resp
+
+    if request.method == 'POST':
+        print(request.get_json(), sys.stderr)
+
+    return resp
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
